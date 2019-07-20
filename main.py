819fb58ff2fb2aee457d7d7ea06a262fb104a2dc
jackcsheehan <jackcsheehan@users.noreply.github.com>
2019-07-20 15:00:51 -0500

    Added license info

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Author: Jack Sheehan
 Software Title: MAVI (Musical Assistance for the Visually Impaired)
 
 Description: A simple peice of software that aids visually impaired muscicians in learning their music. 
-After inputing notes as an RTTTL string, a text-to-speech playback can be downloaded as an MP3 and 
+after inputing notes as an RTTTL string, a text-to-speech playback can be downloaded as an MP3 and 
 replayed anywhere.
 
 Technologies Used:
@@ -14,7 +14,54 @@ Technologies Used:
 	- gTTS 2.0.3 (github.com/pndurette/gTTS)
 
 Copyright(c) 2019 Jack Sheehan
-Licensed under the MIT license.
+
+MAVI License:
+
+MIT License
+
+Copyright (c) 2019 Jack Sheehan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+gTTS License:
+
+The MIT License (MIT)
+
+Copyright © 2014-2018 Pierre Nicolas Durette
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 '''
 
 def main():
@@ -22,5 +69,3 @@ def main():
 	
 if __name__ == "__main__":
 	main()
-
-	
